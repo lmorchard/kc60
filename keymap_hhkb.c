d07ff30a911aae58e0b071c19a122ecceecaf37c
Les Orchard <me@lmorchard.com>
2016-08-08 12:47:17 -0400

    Swap alt & gui like HHKB

--- keymap_hhkb.c
+++ keymap_hhkb.c
@@ -20,7 +20,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TAB,  Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P,    LBRC, RBRC, BSLS, \
         LCTL, A,    S,    D,    F,    G,    H,    J,    K,    L,    SCLN, QUOT,       ENT,  \
         LSFT, Z,    X,    C,    V,    B,    N,    M,    COMM, DOT,  SLSH,             RSFT, \
-        LCTL, LGUI, LALT,             SPC,                          RALT, RGUI, RCTL, FN0),
+        LCTL, LALT, LGUI,             SPC,                          RGUI, RALT, RCTL, FN0),
 
     KEYMAP_ANSI(
         GRV,  F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,   F11,  F12,  DEL,  \
